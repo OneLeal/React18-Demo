419e3d1560d324e91467958993faf06d9f67dffc
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-10-11 01:58:36 +0800

    feat: init project

---
+++ src/components/test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./test.scss";
+
+const TestCom: React.FC = () => {
+  return (
+    <div className="test-wrap">
+      <span className="test-title">Hello World!</span>
+    </div>
+  );
+};
+
+export default TestCom;
