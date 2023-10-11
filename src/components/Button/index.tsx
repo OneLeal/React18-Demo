de5968666990096d3eb7349f5f448bba1d1737fa
Momota <15639067881@163.com>
2023-10-11 18:26:09 +0800

    feat: init dir for Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.scss";
+
+/**
+ * TODO: 
+ * 1. 声明 Button 组件属性;
+ * 2. 集成 Link & Icon 功能;
+ * 3. 样式编写;
+ * 4. 组件交互逻辑编写;
+ * 5. 测试;
+ */ 
+
+const MyButton = () => {
+  return <>Button</>;
+};
+
+export default MyButton;
