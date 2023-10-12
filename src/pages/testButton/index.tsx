8a5ed12487640a4e908351b23d1579bbc3d04735
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-10-13 03:48:14 +0800

    feat: test Button

--- src/pages/testButton/index.tsx
+++ src/pages/testButton/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import MyButton from "../../components/Button";
 
-// 基础组件 Button 测试页面 
+// 基础组件 Button 测试页面
 const TestButton = () => {
-    return <>Test Button Component</>
+  return (
+    <>
+      <h1>Test Button Component</h1>
+
+      <div className="test-page test-page-button">
+        <MyButton type="success">Primary</MyButton>
+
+        <MyButton type="dashed" size="large">
+          Dashed
+        </MyButton>
+      </div>
+    </>
+  );
 };
 
 export default TestButton;
